0212033ab6bfb6f466279f62fba2bf206c66643f
Ronan C <rojocas@gmail.com>
2020-03-10 14:58:18 +0000

    Remove default secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+  import env
 
 if os.environ.get('DEVELOPMENT'):
     development = True
@@ -26,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6!y4ipiza*c(s&alrypbgv(91m_c1=@e&e*2*#-tyw-7#57$sk'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
